4b41c3213c6e27519505af88a2f2cd66f065fd6b
Ryan Daum <ryan.daum@gmail.com>
2025-07-10 22:06:12 -0400

    Double license header fix

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,6 @@
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
 
-// Copyright (C) 2024 Ryan Daum <ryan.daum@gmail.com>
-//
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free Software
-// Foundation, version 3.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with
-// this program. If not, see <https://www.gnu.org/licenses/>.
-//
-
 //! A minimal dependency, minimal feature WASM interpreter meant to be used embedded in other
 //! runtimes.
 //! Will not support WASI or anything fancy, just the bare minimum to run a WASM module without
